8b9217e7c301c6853e765562e84bca641b44b562
Breanna Young <w0459556@campus.nscc.ca>
2024-12-12 22:06:03 -0400

    fix

--- routes/users.js
+++ routes/users.js
@@ -24,7 +24,13 @@ router.get('/', checkAuthToken, async (req, res) => {
 router.post('/register', async (req, res) => {
   const { first_name, last_name, email, password } = req.body;
 
-  // Check password strength
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return res.status(409).json({ message: 'Email already in use' });
+  }
+
+
   const passwordCheck = passwordStrength(password);
   if (passwordCheck.value !== 'Medium' && passwordCheck.value !== 'Strong') {
     return res.status(422).json({
@@ -34,42 +40,38 @@ router.post('/register', async (req, res) => {
   }
 
   try {
-    // Create a new user instance
+  
     const newUser = new User({ first_name, last_name, email, password });
 
-    // Validate user data against the schema
+  
     await newUser.validate();
 
-    // Hash the password
+  
     const hashedPassword = await bcrypt.hash(password, 10);
     newUser.password = hashedPassword;
 
-    // Save the user to the database
+  
     const savedUser = await newUser.save();
 
-    // user authenticated...generate JWT and respond
+  
     const token = jwt.sign({ email: newUser.email }, process.env.JWT_SECRET);
-    res.cookie('userEmail', user.email, { httpOnly: false, path: '/' });
 
-    
+  
+    res.cookie('userEmail', savedUser.email, { httpOnly: false, path: '/' });
     res.cookie('token', token, { httpOnly: true, path: "/" });
+
     res.setHeader('Access-Control-Expose-Headers', '*');
     res.status(201).json({ email: savedUser.email, _id: savedUser._id });
   } catch (err) {
-    if (err.code === 11000) {
-      // duplicate email
-      return res.status(409).json({ message: 'Email already in use' });
-    }
-
+  
     if (err.name === 'ValidationError') {
-      // validation errors
       return res.status(422).json({
         message: 'Validation error',
         details: err.errors,
       });
     }
 
-    // server errors
+  
     res.status(500).json({
       message: 'Server error',
       error: err.message,
@@ -82,18 +84,18 @@ router.post('/register', async (req, res) => {
   router.post('/login', async (req, res) => {
     const { email, password } = req.body;
     try {
-        // validate body including strong password
+      
         await login.validate(req.body);
     
-        // retrieve user with submitted email
+      
         const user = await User.findOne({ email: req.body.email }).exec();
         if (!user) return res.status(401).send();
     
-        // check submitted password against stored hash for a match
+      
         const isMatch = await bcrypt.compare(req.body.password, user.password);
         if (!isMatch) return res.status(401).send();
     
-        // user authenticated...generate JWT and respond
+      
         const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET);
         res.cookie('userEmail', user.email, { httpOnly: false, path: '/' });
 
